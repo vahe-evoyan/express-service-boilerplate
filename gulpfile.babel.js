7a1bfd2aa1d98a3f98824754c2e5b09acf9302a7
Vahe Evoyan <vahe.evoyan@gmail.com>
2017-04-15 02:43:49 +0400

    Move server sources to src directory

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,17 @@ import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 import nodemon from 'nodemon';
 
-const SERVER_PATH = '.';
+const SERVER_PATH = './src';
 
 gulp.task('lint', () => {
-  gulp.src(['**/*.js', '!node_modules/**'])
+  gulp.src([`${SERVER_PATH}/**/*.js`, '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('lint:gulpfile', () => {
+  gulp.src('gulpfile.babel.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
